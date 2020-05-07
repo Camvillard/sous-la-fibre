5c264e97e3d9b2b673529b14aa00c60c537f8658
camille villard <hello@camillevillard.com>
2020-05-07 19:31:02 -0400

    test

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,12 +2,6 @@ import React from "react"
 import Layout from "../components/Layout/Layout.component"
 import SEO from "../components/Seo/Seo.component"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => <>test</>
 
 export default NotFoundPage
