72f801f3feed2b768e498e15594d1962a9d13f0d
camille villard <hello@camillevillard.com>
2020-06-17 12:15:09 -0400

    icons

--- src/components/Footer/Footer.component.tsx
+++ src/components/Footer/Footer.component.tsx
@@ -1,9 +1,69 @@
 import React from "react"
-import { FooterWrapper, Copyright, Credit } from "./Footer.ui"
+import {
+  FaFacebookSquare,
+  FaInstagram,
+  FaSpotify,
+  FaTwitterSquare,
+  FaPodcast,
+} from "react-icons/fa"
+
+import {
+  FooterWrapper,
+  Copyright,
+  FooterIconsWrapper,
+  IconLink,
+} from "./Footer.ui"
 
 export const Footer = () => {
   return (
     <FooterWrapper>
+      <FooterIconsWrapper>
+        <IconLink>
+          <a
+            href="https://podcast.ausha.co/souslafibre"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaPodcast />
+          </a>
+        </IconLink>
+        <IconLink>
+          <a
+            href="https://www.instagram.com/souslafibre/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram />
+          </a>
+        </IconLink>
+        <IconLink>
+          <a
+            href="https://www.facebook.com/souslafibre/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebookSquare />
+          </a>
+        </IconLink>
+        <IconLink>
+          <a
+            href="https://open.spotify.com/show/7vGvdX6m9jpiQtKzRrv6Yx"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaSpotify />
+          </a>
+        </IconLink>
+        <IconLink>
+          <a
+            href="https://twitter.com/Mynamescm_"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitterSquare />
+          </a>
+        </IconLink>
+      </FooterIconsWrapper>
       <Copyright>
         © {new Date().getFullYear()} - sous la fibre | tous droits réservés
       </Copyright>
--- src/components/Footer/Footer.ui.ts
+++ src/components/Footer/Footer.ui.ts
@@ -1,22 +1,39 @@
 import styled from "styled-components"
 import { themeColors } from "../../theme/theme-variables"
 
-const { darkGray, mediumBlue, mediumGray, gray } = themeColors
+const { darkGray, mediumBlue, brightBlue, mediumGray, gray } = themeColors
 
 export const FooterWrapper = styled.footer`
-  border-top: 1px solid ${gray};
-  padding: 4px 0;
+  border-top: 1px solid ${mediumBlue};
+  padding: 16px 0;
   width: 92vw;
   margin-left: auto;
   margin-right: auto;
+`
+
+export const FooterIconsWrapper = styled.ul`
+  list-style: none;
+  margin: 0 0 8px;
+  padding: 0;
+`
+
+export const IconLink = styled.li`
+  display: inline;
+  margin-right: 8px;
+  a {
+    color: ${mediumBlue};
+  }
+  svg {
+    width: 20px !important;
+    height: 20px !important;
+  }
+  path {
+    width: 100%;
+    height: 100%:
+  }
 `
 export const Copyright = styled.p`
   font-size: 1.2rem;
   text-transform: uppercase;
   color: ${gray};
-`
-export const Credit = styled.p`
-  font-size: 1.2rem;
-  text-transform: uppercase;
-  color: ${mediumBlue};
 `
