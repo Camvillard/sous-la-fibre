c803252bc1bf4c610906662876702281373e755a
camille villard <hello@camillevillard.com>
2020-07-22 19:37:07 -0400

    fix(typo): fix typo loading error

--- src/html.js
+++ src/html.js
@@ -11,7 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link href="https://use.typekit.net/pdp7wjj.css" />
+        <link rel="stylesheet" href="https://use.typekit.net/pdp7wjj.css" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
